a4216193a4cd88b66cb8b8fc5fdc4016397db6a5
Puwka <gorokhov.inc@gmail.com>
2018-11-25 20:34:46 +0300

    add serve static files

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
+const path = require('path');
+
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
+const serve = require('koa-static');
 
 const summerRoutes = require('./routes/summer');
 
 const app = new Koa();
-
+// console.log(path.join(__dirname, 'frontend'))
 app.use(bodyParser())
+    .use(serve(path.join(__dirname, '..', '/frontend')))
     .use(cors({
         origin: '*'
     }))
