958b175b037b1d476df2f28ec73fb46d4034a33d
Puwka <gorokhov.inc@gmail.com>
2018-11-26 21:57:27 +0300

    fix backend

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const cors = require('@koa/cors');
+const cors = require('koa-cors');
 const serve = require('koa-static');
 
 const summerRoutes = require('./routes/summer');
@@ -10,6 +10,10 @@ const summerRoutes = require('./routes/summer');
 const app = new Koa();
 // console.log(path.join(__dirname, 'frontend'))
 app.use(bodyParser())
+    .use(async (ctx, next) => {
+        console.log(ctx.url, ctx.method)
+        await next()
+    })
     .use(serve(path.join(__dirname, '..', '/frontend')))
     .use(cors({
         origin: '*'
