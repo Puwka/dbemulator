72c83097416b4876fc3db9d7bd2a39b7096b150d
Anton <Spawni777@gmail.com>
2018-11-26 18:20:06 +0300

    add first form

---
+++ frontend/summer/index.js
@@ -0,0 +1,25 @@
+const inputs = document.querySelectorAll('.selectMe');
+
+inputs.forEach(function(input) {
+    input.addEventListener('blur', function(e) {
+        // console.log(e);
+        const target = e.target;
+        if (target.value) {
+            target.classList.add('focused');
+        } else {
+            target.classList.remove('focused');
+        }
+    })
+});
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', () => {
+    event.preventDefault()
+
+    const data = [].reduce.call(inputs, (acc, input) => {
+        acc[input.id] = input.value
+        return acc
+    }, {});
+
+})
