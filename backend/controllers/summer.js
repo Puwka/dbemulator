924f587275d1146424c22f05a00112b8e40d986b
Puwka <gorokhov.inc@gmail.com>
2018-11-30 21:37:53 +0300

    same name, link assertion

--- backend/controllers/summer.js
+++ backend/controllers/summer.js
@@ -16,6 +16,9 @@ const getToursFromFile = async () => {
 exports.postAddTour = () => async ctx => {
     const payload = ctx.request.body;
     const tours = await getToursFromFile();
+    const {link, id} = tours.find(t => t.link === payload.link);
+    ctx.assert(link !== payload.link, 400, {message: 'Ссылка занята', link});
+    ctx.assert(id !== payload.id, 400, {message: 'Такой тур уже есть', id});
     tours.push(payload);
     writeFileAsync(p, JSON.stringify(tours));
 
